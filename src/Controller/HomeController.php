bc568aa2b183b4ef7da5a3abca5aedb89576c89e
chennuo <chennuo0142@gmail.com>
2024-08-29 13:37:18 +0200

    add

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,11 +8,12 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(DriverRepository $driverRepository, EntityManagerInterface $entityManager): Response
+    public function index(DriverRepository $driverRepository, Request $request): Response
     {
         //on recupere les driver visible
         $drivers = $driverRepository->findBy(['isVisible' => true]);
@@ -23,13 +24,15 @@ class HomeController extends AbstractController
         // }
         // dd($user_moderators);
         // dump($drivers);
+        dump($request->attributes->get("_route"));
         return $this->render('home/index.html.twig', [
             'drivers' => $drivers,
+            'main_route' => $request->attributes->get("_route"),
         ]);
     }
 
     #[Route('/public/driver/{slug}', name: 'app_public_driver_show')]
-    public function public_driver_show($slug, DriverRepository $driverRepository): Response
+    public function public_driver_show($slug, DriverRepository $driverRepository, Request $request): Response
     {
         //on recupere les driver visible
         $driver = $driverRepository->findOneBy([
@@ -37,8 +40,24 @@ class HomeController extends AbstractController
             'slug' => $slug
         ]);
         dump($driver);
+        dump($request->attributes->get("_route"));
         return $this->render('home/public/driver_show.html.twig', [
             'driver' => $driver,
+        ]);
+    }
+
+    #[Route('/public/drivers/show', name: 'app_public_all_drivers_show')]
+    public function public_all_driver_show(DriverRepository $driverRepository, Request $request): Response
+    {
+        //on recupere les driver visible
+        $drivers = $driverRepository->findBy([
+            'isVisible' => true,
+
+        ]);
+        dump($drivers);
+        dump($request);
+        return $this->render('home/public/all_drivers_show.html.twig', [
+            'drivers' => $drivers,
         ]);
     }
 }
